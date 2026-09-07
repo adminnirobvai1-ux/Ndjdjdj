893f3076f35a5acc27382468abd01b4b6e8f2ff3
adminnirobvai1-ux <adminnirobvai1@gmail.com>
2026-09-07 09:31:52 +0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -26,7 +26,7 @@ from telebot import types
 # =========================================================
 # CONFIGURATION
 # =========================================================
-BOT_TOKEN = "8949748635:AAF9w3mFRx2fqcE6AslsrR7AUuNJQzqB-PA"
+BOT_TOKEN = "8546395043:AAE0nQWcTuH8mFflnL3Qp8y6rD5XJUefMQk"
 API_URL = "https://draw.ar-lottery01.com/WinGo/WinGo_30S/GetHistoryIssuePage.json"
 MARKET_INTERVAL = 30   # ৩০ সেকেন্ড মার্কেট
 TOTAL_PAGES = 50       # ৫০ পেজ (প্রতি পেজে ১০টি করে ৫০০ রেকর্ড)
